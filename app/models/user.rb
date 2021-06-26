391d40030f67f06fa91d1c3b5838412cdb9da438
ittetu981 <84568432+ittetu981@users.noreply.github.com>
2021-06-26 15:44:09 +0900

    新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :
+  validates :full_width_surname, presence: true
+  validates :surname_yomigana, presence: true
+  validates :full_width_name, presence: true
+  validates :name_yomigana, presence: true
+  validates :birthday, presence: true
+  validates :nickname, presence: true
 
-
-
-
+  has_many :items
+  has_many :purchases
 end

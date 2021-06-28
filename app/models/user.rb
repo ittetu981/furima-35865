3e4a08522df3fb2df3486200eda66f685b338b17
ittetu981 <84568432+ittetu981@users.noreply.github.com>
2021-06-28 17:39:42 +0900

    追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, } do
+
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, } do
     validates :full_width_surname
     validates :full_width_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, } do
     validates :surname_yomigana
     validates :name_yomigana
   end
-  validates :full_width_surname, presence: true
-  validates :surname_yomigana, presence: true
-  validates :full_width_name, presence: true
-  validates :name_yomigana, presence: true
+
   validates :birthday, presence: true
   validates :nickname, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
   has_many :items
   has_many :purchases
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'1a' + Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     full_width_surname    {"田中"}
     full_width_name       {"葵"}

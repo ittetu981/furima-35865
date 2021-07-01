4ba393dffbaf04a6d7706ac846c445dada93123b
ittetu981 <84568432+ittetu981@users.noreply.github.com>
2021-07-01 21:35:46 +0900

    Revert "Merge pull request #4 from ittetu981/商品出品機能"
    This reverts commit d61ba6dfa5735ad451c8baf47a6a958435be0439, reversing
    changes made to b1af5ec87bafdbdef0c7c47b3ef1fd25c6ae122f.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,4 @@
 class ItemsController < ApplicationController
-
-  before_action :authenticate_user!, except:[:index]
-
   def index
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:product_name, :description_of_item, :category_id, :delivery_charge_id, :product_status_id, :shipment_source_id, :price, :shipping_time_id, :image).merge(user_id: current_user.id)
   end
 end
